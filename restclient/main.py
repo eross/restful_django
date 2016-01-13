bf07a908d2255793f7d85f189297be83218150d8
Eric Ross <eric.ross@hp.com>
2016-01-13 15:54:26 -0800

    still does not PUT

--- restclient/main.py
+++ restclient/main.py
@@ -1,6 +1,8 @@
 import requests
 
+
 r = requests.get('http://localhost:8000/sas/users/?format=json')
+
 if r.status_code == 200:
 	print r.text
 	val = r.json()
@@ -9,21 +11,29 @@ if r.status_code == 200:
 		index = index + 1
 		print rec['username'],': ',rec['email'],': ',rec['url']
  
-payload = {'username':'ericr3', 'email': 'xyz@contosu.com', 'groups': [u'http://localhost:8000/sas/groups/1/?format=json']}
+payload = {'username':'ericr2', 'email': 'xyz@contosu.com', 'groups': [u'http://localhost:8000/sas/groups/1/?format=json']}
 
 #r = requests.post('http://localhost:8000/sas/users/',json=payload)
 #print r.status_code
 #print r.text
 
-payload = {
-    "url": "http://localhost:8000/sas/users/4/",
-    "username": "ericr3",
-    "email": "xyz@contosu.com",
-    "groups": [
-        "http://localhost:8000/sas/groups/1/"
-    ]
-}
-#payload = {"username":"ericr3b"}
-r = requests.put('http://localhost:8000/sas/users/4',json=payload)
-print r.status_code
-print r.text
+#payload = {
+#    "url": "http://localhost:8000/sas/users/4/",
+#    "username": "ericr3",
+#   "email": "xyz@contosu.com",
+#    "groups": [
+#        "http://localhost:8000/sas/groups/1/"
+#    ]
+#}
+payload = {"username":"ericr3b"}
+headers = {'Content-Type': 'application/json'}
+#r = requests.post('http://localhost:8000/sas/users/',json=payload)
+
+r = requests.put('http://localhost:8000/sas/users/4',json=payload, auth=('a','a'))
+if r.status_code == 200:
+	print r.status_code
+	print r.text
+else:
+	print "status: ",r.status_code
+	print "Could not update record"
+	print r.text
